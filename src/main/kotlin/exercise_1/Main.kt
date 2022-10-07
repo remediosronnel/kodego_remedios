cdf918a727b55cd6f73092cde443b861d1bab7db
Ronnel <remediosronnel30@gmail.com>
2022-10-07 23:57:55 +0800

    added exercise 1 package added Main.kt in exercise_1

--- src/main/kotlin/exercise_1/Main.kt
+++ src/main/kotlin/exercise_1/Main.kt
@@ -1,39 +1,41 @@
 package exercise_1
 
+import java.time.LocalDateTime
 
 
 fun main(args: Array<String>) {
+    var poSition: String = ""
+    var desiredSalary:String = ""
+    var dateAvailable:String = ""
+
+
 //PERSONAL INFORMATION
 
-    var lastName:String = "Basilio      "
-    var firstName:String = "Bubbles     "
-    var middleName:String = "Parcon"
-    var Address:String = "Purok 7, Poblacion"
-    var ciTy:String = "Bayugan City"
-    var stAte:String = "Caraga Region"
+    var lastName:String = ""
+    var firstName:String = ""
+    var middleName:String = ""
+    var Address:String = ""
+    var ciTy:String = ""
+    var stAte:String = ""
     var zIp:Int = 8965
 
-    var homePhone:String?= "456-5897"
-    var cellPhone:String?= "093798549645"
-    var emailAddress:String= "hagorn_peranha@gmail.com"
-    var ssNumber:String= "45K890A087D889"
-    var citizen:String ?= "YES"
-    var felony:String ?= "NO"
-    var preEmployment:String ?= "YES"
+    var homePhone:String?= ""
+    var cellPhone:String?= ""
+    var emailAddress:String= ""
+    var ssNumber:String= ""
+    var citizen:String ?= ""
+    var felony:String ?= ""
+    var preEmployment:String ?= ""
 
     var a = "$lastName $firstName $middleName"
     val b = "\n $Address, $ciTy, $stAte, $zIp"
-    val c = "\n$homePhone $cellPhone          $emailAddress  $ssNumber        $citizen             $felony           $preEmployment"
+    val c = "\n$homePhone $cellPhone $emailAddress $ssNumber $citizen $felony $preEmployment"
 
     println(a)
-    println("Last Name   First Name    Middle Name")
     println(b)
-    println("     Address            City            State    Zip")
     println(c)
-    println("Telephone  Cellphone No.       Email Add                    SSS No.         US Citizen?     Are you?      Are you?")
-
     age()
-
+    Education()
 }
 
 fun Education(){
@@ -43,9 +45,6 @@ fun Education(){
     var yearsAttended:String = ""
     var degreeReceived:String = ""
     var maJor:String = ""
-
-
-
 }
 
 fun age() {
