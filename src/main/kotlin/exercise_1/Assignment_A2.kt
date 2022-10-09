7cca1e8de5980783f9cd0fee63956c98b8d4753d
Ronnel <remediosronnel30@gmail.com>
2022-10-09 10:18:54 +0800

    added exercise 1 package added Main.kt in exercise_1

---
+++ src/main/kotlin/exercise_1/Assignment_A2.kt
@@ -0,0 +1,58 @@
+package exercise_1
+
+
+import java.time.*
+import java.time.format.DateTimeFormatter.*
+
+fun main() {
+
+
+//    Student Information
+    var nameOfBorrower: String = ""
+    var studentIDNo: String = ""
+    var studentCourse: String = ""
+    var studentDepartment: String = ""
+    var studentCollege: String = ""
+
+//    Book Information
+    var bookIDNumber: Int? = null
+    var bookSubject: String = ""
+    var bookTitle: String = ""
+    var authorName: String = ""
+    var nameOfPublisher: String = ""
+    var copyRightfortheBook: String = ""
+    var editionNumber: Int? = null
+    var isbnofBook: Int? = null
+    var shelfNumberBook: Int? = null
+
+    var newBookIdNumber = bookIDNumber.toString()
+    var bookIDTypeCast = newBookIdNumber as String
+
+
+
+    if (bookIDTypeCast is String) {
+        println("String ID Number: $bookIDTypeCast")
+    } else {
+        println("Integer ID Number: $bookIDTypeCast")
+    }
+
+    bookReturn()
+}
+
+fun bookReturn(){
+    var period = Period.of(0, 0, 3)
+    var todayDate = LocalDate.of(2022, 10, 9)
+    var modifiedDate = todayDate.plus(period)
+
+    if(LocalDate.now() >= modifiedDate){
+        println("Please return the book")
+    }
+    else{
+        println("Please continue reading")
+    }
+
+
+
+
+}
+
