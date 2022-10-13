de9c7e6a757b5ccfbed734eb6d025107c4e2b1f8
Ronnel <remediosronnel30@gmail.com>
2022-10-13 12:28:37 +0800

    added exercise 1 package added Main.kt in exercise_1

--- src/main/kotlin/exercise_1/Activity1_D.kt
+++ src/main/kotlin/exercise_1/Activity1_D.kt
@@ -1,11 +1,4 @@
 package exercise_1
-import com.sun.jdi.connect.Connector.IntegerArgument
-import com.sun.jdi.connect.Connector.SelectedArgument
-import java.awt.HeadlessException
-import java.lang.NumberFormatException
-import java.text.DecimalFormat
-import java.text.DecimalFormatSymbols
-import  kotlin.Float.Companion.POSITIVE_INFINITY as inf
 
 
 
@@ -17,25 +10,26 @@ fun main() {
 
     println("Enter 5 Monetary Amounts")
     var numbersIn = 1..100000000000000000
+    var letterS:CharRange = ('A'..'z')
     var amount1:Int = readln().toInt()
     var amount2:Int = readln().toInt()
     var amount3:Int = readln().toInt()
     var amount4:Int = readln().toInt()
     var amount5:Int = readln().toInt()
 
-        if ((amount1 in numbersIn == true) and (amount2 in numbersIn == true) and (amount3 in numbersIn == true) and (amount4 in numbersIn == true) and (amount5 in numbersIn == true))
+        if ((amount1 and amount2 and amount3 and amount4 and amount5) in numbersIn)
     {
         var quotientNumber = amount1!!.div(5).toFloat()
            print("Result: " + quotientNumber)
     }
-        else{
+        else {
         println("Wrong Input")
     }
 
 }
 
-//Another Version - UGA ANG UTOK!!heheheheh
-
+//******Another Version - UGA ANG UTOK!!!*******
+//
 //fun main() {
 //
 //    try {
@@ -50,7 +44,7 @@ fun main() {
 //        println("Wrong Inputs, it should be amount")
 //    }
 //}
-
+//
 
 
 
